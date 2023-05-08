5cccba628996adfe62fde425221960449cac9520
AvinFajarF <avinfajar6@gmail.com>
2023-05-08 10:50:49 +0700

    commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,6 +8,7 @@
     <link rel="stylesheet" href="{!! asset('assets/css/style.css') !!}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 </head>
 
 <body>
@@ -154,24 +155,36 @@
                     <h6 class="hero-paragraf">Why choose us</h6>
                     <h1 class="fw-bold w-100">Get the best experience of rental cars</h1>
 
-                    <div class="point d-flex flex-column mt-5">
+                    <div class="point mt-5">
 
-                        <div class="cheapest">
-                            {{-- <img src="{!! asset('assets/img/dompet.png') !!}" class="dompet" alt="" srcset=""> --}}
-                            <h6 class="fw-bold">Cheapest Market Price Guaranteed</h6>
-                            <p class="hero-paragraf">We refund 100% if you find cheaper alternative</p>
+                        <div class="cheapest d-flex align-items-center">
+                            <img src="{!! asset('assets/img/dompet.png') !!}" class="dompet" alt="" srcset="">
+                            <div>
+                                <h6 class="fw-bold">Cheapest Market Price Guaranteed</h6>
+                                <p class="hero-paragraf">We refund 100% if you find cheaper alternative</p>
+                            </div>
                         </div>
-                        <div class="hire-drive">
-                            <h6 class="fw-bold">Hire Driver</h6>
-                            <p class="hero-paragraf">Don’t have a driver? We got you covered with that too.</p>
+                        <div class="hire-drive d-flex align-items-center">
+                            <img src="{!! asset('assets/img/user.png') !!}" alt="" srcset="">
+                            <div>
+                                <h6 class="fw-bold">Hire Driver</h6>
+                                <p class="hero-paragraf">Don’t have a driver? We got you covered with that too.</p>
+                            </div>
                         </div>
-                        <div class="same-day">
-                            <h6 class="fw-bold">Same Day Delivery</h6>
-                            <p class="hero-paragraf">Book and we will deliver it directly to you within 24 Hours</p>
+                        <div class="same-day d-flex align-items-center">
+                            <img src="{!! asset('assets/img/24-jam.png') !!}" alt="" srcset="">
+                            <div>
+                                <h6 class="fw-bold">Same Day Delivery</h6>
+                                <p class="hero-paragraf">Book and we will deliver it directly to you within 24 Hours
+                                </p>
+                            </div>
                         </div>
-                        <div class="support">
-                            <h6 class="fw-bold">24/7 Support</h6>
-                            <p class="hero-paragraf">Contact us if you have any issues</p>
+                        <div class="support d-flex align-items-center">
+                            <img src="{!! asset('assets/img/support.png') !!}" alt="" srcset="">
+                            <div>
+                                <h6 class="fw-bold">24/7 Support</h6>
+                                <p class="hero-paragraf">Contact us if you have any issues</p>
+                            </div>
                         </div>
 
                     </div>
@@ -194,33 +207,109 @@
     {{-- end Why choose us --}}
 
 
+    {{-- popular rent car --}}
 
+    <div class="pupular-rent-car mt-5">
+
+        <h5 class="hero-paragraf text-center">Popular rental deals</h5>
+        <h2 class="fw-bold text-center">Most popular cars rental deals</h2>
+
+        {{-- <div class="row">
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="{!! asset("assets/img/Noida.png") !!}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">i20 2018 Model - Noida, India</h5>
+                    <div class="row">
+                        <div class="col  d-flex align-items-center">
+                            <img src="{!! asset("assets/img/ac.png") !!}" alt="" srcset="">
+                            <div>
+                                <p class="text-description mt-2">Air Conditioner</p>
+                            </div>
+                        </div>
+                        <div class="col">
+
+                        </div>
+                        <div class="col">
+
+                        </div>
+                        <div class="col">
+
+                        </div>
+                    </div>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
+
+            </div>
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
+            </div>
+        </div> --}}
 
 
     </div>
 
 
+    </div>
 
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
+    <footer>
+        <div class="container">
+
+            <div class="row">
+                <div class="col">
+                    <img src="{!! asset('assets/img/carmania-black.png') !!}" alt="" srcset="">
+                </div>
+                <div class="col d-flex align-items-center">
+                    <p class="text-white">Casa Woodstock, Shop no. 12, Gaur City, Ghaziabad - 201301</p>
+                </div>
+                <div class="col d-flex align-items-center">
+                    <img src="{!! asset('assets/img/icon-phone.png') !!}" alt="" srcset="">
+                    <p class="text-white fw-bold">+91-9140656920</p>
+                </div>
+                <div class="col d-flex align-items-center flex-column follow-us">
+                    <p class="text-white fw-bold">Follow Us</p>
+                    <div class="d-flex justify-content-end align-items-center">
+                        <img src="{!! asset('assets/img/twitter.png') !!}" alt="" srcset="">
+                        <img src="{!! asset('assets/img/facebook.png') !!}" alt="" srcset="">
+                        <img src="{!! asset('assets/img/instagram.png') !!}" alt="" srcset="">
+                        <img src="{!! asset('assets/img/youtube.png') !!}" alt="" srcset="">
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <hr class="garis">
+        <p class="text-center text-white py-5">Copyright 2022 © Dhruv Dikshit, All Rights Reserved.</p>
+    </footer>
+
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
